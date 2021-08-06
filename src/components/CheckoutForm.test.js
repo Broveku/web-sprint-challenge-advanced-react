bb37b9b889623424a23cc8a7f1a97d36b3ff9710
Broveku <gearhearjordan@gmail.com>
2021-08-06 13:55:38 -0700

    finished sprint project

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,10 +1,41 @@
 import React from "react";
-import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("renders without errors", () => {});
+test("renders without errors", () => {
+    render(<CheckoutForm/>)
+    const header = screen.queryByText(/Checkout Form/i)
+    expect(header).toBeInTheDocument()
+});
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", () => {
+    render(<CheckoutForm />)
+    
+    const firstNameInput = screen.getByLabelText(/first name:/i)
+    userEvent.type(firstNameInput, 'Jordan')
+
+    const lastNameInput = screen.getByLabelText(/last name:/i)
+    userEvent.type(lastNameInput, 'Gearheart')
+
+    const addressInput = screen.getByLabelText(/address:/i)
+    userEvent.type(addressInput, '1234 Cotton Rd')
+
+    const cityInput = screen.getByLabelText(/city:/i)
+    userEvent.type(cityInput, 'Kailua-Kona')
+
+    const stateInput = screen.getByLabelText(/state:/i)
+    userEvent.type(stateInput, 'Hawaii')
+
+    const zipInput = screen.getByLabelText(/zip:/i)
+    userEvent.type(zipInput, '96740')
+
+    const submitBtn = screen.getByRole('button')
+    userEvent.click(submitBtn)
+
+    const successMsg = screen.getByTestId(/successMessage/i)
+    expect(successMsg).toBeInTheDocument()
+
+});
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,32 @@
-// write your custom hook here to control your checkout form
+import { useState } from 'react'
+
+
+const initialValue = {
+    firstName: "",
+    lastName: "",
+    address: "",
+    city: "",
+    state: "",
+    zip: "",
+  };
+
+  const useForm = (props) => {
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [values, setValues] = useState(initialValue);
+  
+ 
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
+
+    return [
+        showSuccessMessage,
+        setShowSuccessMessage,
+        values,
+        setValues,
+        handleChanges
+
+    ]
+}
+
+export default useForm 
